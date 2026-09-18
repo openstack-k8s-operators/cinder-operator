e61f4dfe668d5dd7ab7fa4b9db6e066132326fa7
Francesco Pantano <fpantano@redhat.com>
2026-09-18 17:56:53 +0200

    Update Cinder related images to S2I containers
    Switch the related image defaults and samples from
    quay.io/openstack-k8s-operators to the dedicated
    quay.io/openstack-s2i-containers namespace, where
    the s2i generated content is published.
    
    Signed-off-by: Francesco Pantano <fpantano@redhat.com>

--- api/v1beta1/cinder_types.go
+++ api/v1beta1/cinder_types.go
@@ -37,13 +37,13 @@ const (
 	// Container image fall-back defaults
 
 	// CinderAPIContainerImage is the fall-back container image for CinderAPI
-	CinderAPIContainerImage = "quay.io/openstack-k8s-operators/openstack-cinder-api:master-latest"
+	CinderAPIContainerImage = "quay.io/openstack-s2i-containers/openstack-cinder-api:master-latest"
 	// CinderBackupContainerImage is the fall-back container image for CinderBackup
-	CinderBackupContainerImage = "quay.io/openstack-k8s-operators/openstack-cinder-backup:master-latest"
+	CinderBackupContainerImage = "quay.io/openstack-s2i-containers/openstack-cinder-backup:master-latest"
 	// CinderSchedulerContainerImage is the fall-back container image for CinderScheduler
-	CinderSchedulerContainerImage = "quay.io/openstack-k8s-operators/openstack-cinder-scheduler:master-latest"
+	CinderSchedulerContainerImage = "quay.io/openstack-s2i-containers/openstack-cinder-scheduler:master-latest"
 	// CinderVolumeContainerImage is the fall-back container image for CinderVolume
-	CinderVolumeContainerImage = "quay.io/openstack-k8s-operators/openstack-cinder-volume:master-latest"
+	CinderVolumeContainerImage = "quay.io/openstack-s2i-containers/openstack-cinder-volume:master-latest"
 
 	// DBPurgeDefaultAge - Default age, in days, for purging deleted DB records
 	DBPurgeDefaultAge = 30
